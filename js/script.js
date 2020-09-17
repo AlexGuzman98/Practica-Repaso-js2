583380515a295301df29657861fcf92bf3525b05
Alex D. Guzman <alexd.guzman98@gmail.com>
2020-09-16 22:03:28 -0600

    repaso

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-function clickit(event){
-    
-}
+const enlaces = document.querySelectorAll('.tabs-tablist-item');
 
-const enlace = document.querySelectorAll('.tabs-tablist-item');
 const contenido = document.querySelectorAll('.tabs-content-item');
 for(let i = 0; i < contenido.length; i++){
-    contenido[i].style.display = 'none';
+    contenido[i].classList.add('js-content-hidden');
 }
-for(let i = 0; i < enlace.length; i++){
-    enlace[i].addEventListener('click', clickit);
-}
+contenido[0].classList.remove('js-content-hidden');
+enlaces[0].classList.add('js-active');
 
+for (let i = 0; i < enlace.length; i++){
+    enlace[i].addEventListener('click', (event) => {
+        console.log(event);
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
+        
+        for (let i = 0; i <  contenido.length; i++){
+            contenido[i].classList.add('js-content-hidden');
+        if (contenido[i].getAttribute('id')==id) {
+            contenido[i].classList.remove('js-content-hidden');
+            
+    }
+}
+for (let i = 0; i < enlace.length; i++) {
+    enlaces[i].classList.remove('js-active');
+}
+event.currentTarget.classList.add('js-active');
+    });
+}
